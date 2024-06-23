9cda416b043f91439bad46a722be95034870aa57
yahoo168 <a86305394@gmail.com>
2024-06-23 20:50:53 +0800

    add requirement and procfile for heroku

--- app/external_tools/__init__.py
+++ app/external_tools/__init__.py
@@ -16,8 +16,12 @@ module_path = os.path.join("/Users/yahoo168/Desktop/資料庫_測試功能")
 if module_path not in sys.path:
     sys.path.append(module_path)
 
-# 现在可以导入 external_modules 目录下的模块
-from self_finance_database_tool_cloud import google_tools # type: ignore
+# 導入github中 alphahelix-database-cloud的module
+import google_tools  # type: ignore
+import readwise_tools  # type: ignore
 
-# 导出 google_tools 模块
-__all__ = ['google_tools', 'readwise_tools']
+# from alphahelix-database-cloud import google_tools # type: ignore
+# from alphahelix-database-cloud import readwise # type: ignore
+
+# # 导出 google_tools 模块
+# __all__ = ['google_tools', 'readwise_tools']
